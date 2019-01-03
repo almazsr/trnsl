119492a355256853090146c4991d4afd549da8f5
almazsr <almazsr@gmail.com>
2019-01-04 00:48:39 +0300

    Icons added

--- src/chrome/popup.js
+++ src/chrome/popup.js
@@ -2,9 +2,9 @@ $(function() {
 	chrome.storage.sync.get({"translations" : {}}, function(storageResponse) {
 	  var translations = storageResponse.translations;
 	  Object.keys(translations).forEach(function(key, index) {
-		  $('#translations').append(`<div style='margin: 2px;'><button>X</button> <span class='source'>${key}</span> - <span class='result'>${translations[key]}</span></div>`);
+		  $('#translations').append(`<div style='margin: 2px;'><img src="remove.png" style="width: 16px; vertical-align:middle"/> <span class='source'>${key}</span> - <span class='result'>${translations[key]}</span></div>`);
 	  });	
-		$("#translations > div > button").click(function (){			
+		$("#translations > div > img").click(function (){			
 			var source = $(this).parent().find('span.source').html();
 			delete translations[source];
 			chrome.storage.sync.set(storageResponse);
